872f7e696514b375364acee4f0769fe844861944
Supre-le-sucre <wilhemblondel@gmail.com>
2025-05-04 20:47:12 +0200

    Added river.m4a

--- app/src/main/java/fr/supre/cardiac_coherence/Sounds.kt
+++ app/src/main/java/fr/supre/cardiac_coherence/Sounds.kt
@@ -3,10 +3,10 @@ package fr.supre.cardiac_coherence
 enum class Sounds(val stringToShow: String, val resId: Int) {
 
     WAVES("Vagues", R.raw.waves),
+    RIVER("Rivière", R.raw.river),
     FIREPLACE("Feu de bois", R.raw.fireplace),
     BIRDS("Chants d'oiseaux", R.raw.birdschirping),
     RAIN("Pluie", R.raw.rain),
     THUNDER("Orage", R.raw.thunderstorm),
     CAT("Ronronnement", R.raw.cat)
-
 }
